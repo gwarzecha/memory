0f03aa3a16b2bce4d62ba85d3020edb86ed7432a
gwarz <gmwarzecha@tutanota.com>
2022-01-13 20:38:31 -0700

    create SingleCard component, set up card choice click logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImgs = [
   { src: "/img/helmet-1.png" },
@@ -13,25 +14,35 @@ const cardImgs = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImgs, ...cardImgs]
       // returns num between 0 and 1, subtracting 0.5 gives either neg or pos num
       .sort(() => Math.random() - 0.5)
-      // spreads all properties of the card, adds an id to each card 
+      // spreads all properties of the card, adds an id to each card
       .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
   return (
     <div className="App">
       <h1>Memory Match</h1>
       <button onClick={shuffleCards}>New Game!</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+        ))}
+      </div>
     </div>
   );
 }
